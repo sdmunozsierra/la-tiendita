9c9c7c2964b6ea35104730acd10af78e7927f775
Mike <mike@mikes-macbook-pro.local>
2018-10-07 00:55:13 -0800

    Initial Commit

---
+++ la-tiendita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  la-tiendita
+//
+//  Created by Mike on 7/10/18.
+//  Copyright © 2018 Sdmunoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
